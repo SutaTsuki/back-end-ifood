8315a43b511e70e866e1270b7dedcf64bc82043a
Pedro Stabach <pedroaugustostabach@gmail.com>
2025-03-06 17:14:33 -0300

    teste erro

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const path = require("path");
+const http = require("http");
 const app = express();
+const fs = require("fs");
 
+http.createServer(function(req, res) {
+    if(req.url === "/") {
+        fs.readFile("index.html", function(err, data){
+            res.writeHead(200, {"content-type" : "text-html"});
+            res.end(data);
+        });
+    }
+});
 
-
-
-
-
-
+console.log("O SERVIDOR ESTA RODANDO");
 app.listen(8080);
