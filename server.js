95377de5324cd1e5750570b05729c812aa16ddd6
Pedro Stabach <pedroaugustostabach@gmail.com>
2025-03-06 21:41:55 -0300

    GPPD backend

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const public = path.join(__dirname);
+const login = path.join(__dirname);
 
 app.use("/", express.static(public));
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(public, "index.html"));
-})
+});
+
+app.get("/login", function(req,res) {
+    res.sendFile(path.join(public, "login.html" ));
+});
 console.log("o servidor esta rodando");
 app.listen(8080);
